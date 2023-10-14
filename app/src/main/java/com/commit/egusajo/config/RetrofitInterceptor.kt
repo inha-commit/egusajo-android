e14aea3db2880f45ceff1a49eb9da206db0c5cce
박진성 <plashdof@gmail.com>
2023-10-15 00:10:10 +0900

    #1 feat : RetrofitInterceptor 구현

---
+++ app/src/main/java/com/commit/egusajo/config/RetrofitInterceptor.kt
@@ -0,0 +1,25 @@
+package com.commit.egusajo.config
+
+import com.commit.egusajo.App.Companion.sharedPreferences
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+
+class RetrofitInterceptor() : Interceptor {
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder: Request.Builder = chain.request().newBuilder()
+        val jwt: String? = sharedPreferences.getString(X_ACCESS_TOKEN, null)
+        jwt?.let{
+            builder.addHeader("Authorization", jwt)
+        }?:run{
+            // 로컬에 저장된 토큰 없는경우
+        }
+
+        return chain.proceed(builder.build())
+    }
+
+
+}
