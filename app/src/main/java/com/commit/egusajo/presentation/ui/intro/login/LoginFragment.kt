2bf464b1743e1bf65c2b6355dc118e10e2bf825c
박진성 <plashdof@gmail.com>
2023-10-23 06:02:14 +0900

    #4 feat : nav graph 리소스 추가, LoginFragment 생성

---
+++ app/src/main/java/com/commit/egusajo/presentation/ui/intro/login/LoginFragment.kt
@@ -0,0 +1,10 @@
+package com.commit.egusajo.presentation.ui.intro.login
+
+import com.commit.egusajo.R
+import com.commit.egusajo.databinding.FragmentLoginBinding
+import com.commit.egusajo.presentation.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login){
+
+
+}
