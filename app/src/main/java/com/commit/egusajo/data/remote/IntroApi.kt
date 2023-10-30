a8d79c05a7eadbcc6998c75bd59ace8dd9b4ec5a
박진성 <plashdof@gmail.com>
2023-10-30 20:50:21 +0900

    #5 feat : Signup, NickCheck API 구현

--- app/src/main/java/com/commit/egusajo/data/remote/IntroApi.kt
+++ app/src/main/java/com/commit/egusajo/data/remote/IntroApi.kt
@@ -2,6 +2,9 @@ package com.commit.egusajo.data.remote
 
 import com.commit.egusajo.data.model.LoginRequest
 import com.commit.egusajo.data.model.LoginResponse
+import com.commit.egusajo.data.model.NickCheckRequest
+import com.commit.egusajo.data.model.NickCheckResponse
+import com.commit.egusajo.data.model.SignupRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,5 +14,15 @@ interface IntroApi {
     @POST("/auth/sign-in")
     suspend fun login(
         @Body params: LoginRequest
-    ) : Response<LoginResponse>
+    ): Response<LoginResponse>
+
+    @POST("/auth/sign-up")
+    suspend fun signup(
+        @Body params: SignupRequest
+    ): Response<LoginResponse>
+
+    @POST("/auth/nickname-validation")
+    suspend fun checkNick(
+        @Body params: NickCheckRequest
+    ): Response<NickCheckResponse>
 }
--- app/src/main/java/com/commit/egusajo/data/repository/IntroRepositoryImpl.kt
+++ app/src/main/java/com/commit/egusajo/data/repository/IntroRepositoryImpl.kt
@@ -2,6 +2,9 @@ package com.commit.egusajo.data.repository
 
 import com.commit.egusajo.data.model.LoginRequest
 import com.commit.egusajo.data.model.LoginResponse
+import com.commit.egusajo.data.model.NickCheckRequest
+import com.commit.egusajo.data.model.NickCheckResponse
+import com.commit.egusajo.data.model.SignupRequest
 import com.commit.egusajo.data.remote.IntroApi
 import retrofit2.Response
 import javax.inject.Inject
@@ -9,5 +12,6 @@ import javax.inject.Inject
 class IntroRepositoryImpl @Inject constructor(private val api: IntroApi) : IntroRepository {
 
     override suspend fun login(data: LoginRequest): Response<LoginResponse> = api.login(data)
-
+    override suspend fun signup(data: SignupRequest): Response<LoginResponse> = api.signup(data)
+    override suspend fun checkNick(data: NickCheckRequest): Response<NickCheckResponse> = api.checkNick(data)
 }
