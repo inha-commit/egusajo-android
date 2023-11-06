3279da578a8256d505ca5be5af41353ca3a0409b
박진성 <plashdof@gmail.com>
2023-11-06 19:41:31 +0900

    #6 feat : Signup 로직 변경, AccountVerificationFragment 추가

---
+++ app/src/main/java/com/commit/egusajo/presentation/ui/intro/signup/AccountVerificationFragment.kt
@@ -0,0 +1,16 @@
+package com.commit.egusajo.presentation.ui.intro.signup
+
+import android.os.Bundle
+import android.view.View
+import com.commit.egusajo.R
+import com.commit.egusajo.databinding.FragmentAccountVerificationBinding
+import com.commit.egusajo.presentation.base.BaseFragment
+
+class AccountVerificationFragment: BaseFragment<FragmentAccountVerificationBinding>(R.layout.fragment_account_verification) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
