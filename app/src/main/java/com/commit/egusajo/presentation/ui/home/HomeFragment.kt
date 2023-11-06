ff218aec81d30c34f13fd054b7f0f29a20ae597a
박진성 <plashdof@gmail.com>
2023-11-06 18:43:10 +0900

    #6 feat : HomeFragment DataBinding 주입, HomeViewModel 생성

--- app/src/main/java/com/commit/egusajo/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/commit/egusajo/presentation/ui/home/HomeFragment.kt
@@ -1,8 +1,21 @@
 package com.commit.egusajo.presentation.ui.home
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.commit.egusajo.R
 import com.commit.egusajo.databinding.FragmentHomeBinding
 import com.commit.egusajo.presentation.base.BaseFragment
+import com.commit.egusajo.presentation.ui.home.adapter.HomeFundAdapter
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
+        binding.rvFund.adapter = HomeFundAdapter()
+    }
 }
