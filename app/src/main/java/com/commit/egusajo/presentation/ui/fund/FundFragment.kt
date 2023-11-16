59614a8ee1038c6386c523b22ee29d32b1265a45
박진성 <plashdof@gmail.com>
2023-11-17 08:14:34 +0900

    #19 feat : 펀딩 메인 -> 펀딩 생성하기 페이지 이동로직 구현

--- app/src/main/java/com/commit/egusajo/presentation/ui/fund/FundFragment.kt
+++ app/src/main/java/com/commit/egusajo/presentation/ui/fund/FundFragment.kt
@@ -2,6 +2,8 @@ package com.commit.egusajo.presentation.ui.fund
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.commit.egusajo.R
 import com.commit.egusajo.databinding.FragmentFundBinding
@@ -12,9 +14,25 @@ class FundFragment : BaseFragment<FragmentFundBinding>(R.layout.fragment_fund) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setGif()
+        setBtnListener()
+    }
+
+    private fun setGif() {
         Glide.with(this)
             .load(R.raw.ballon)
-            .override(150,150)
+            .override(150, 150)
             .into(binding.ivHeader)
+    }
+
+    private fun setBtnListener() {
+        binding.btnCreateFund.setOnClickListener {
+            findNavController().toCreateFund()
+        }
+    }
+
+    private fun NavController.toCreateFund() {
+        val action = FundFragmentDirections.actionFundFragmentToCreateFundFragment()
+        this.navigate(action)
     }
 }
